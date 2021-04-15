551efa892b941c91461d2cfd10fe1043b9a07cc2
TechLabsInc <76017200+TechLabsInc@users.noreply.github.com>
2021-04-15 14:31:47 -0600

    Added Fake Sur filesystem mounting, CoreGraphics, Updated AppKit, started framework for modular apps, fixed autoplay issues and added favicon.

--- projects/fake-sur/boot/kernel.js
+++ projects/fake-sur/boot/kernel.js
@@ -1,81 +1,128 @@
 var bootVolume = "Macintosh HD";
 var kernel = {
 
-    console: [],
-    log: function (msg) {
-        kernel.console.push(msg.toString());
-        console.log(msg);
-    },
+	console: [],
+	log: function (msg) {
+		kernel.console.push(String(msg));
+		console.log(msg);
+	},
 
-    readFile: function (file, callback) {
-        var rawFile = new XMLHttpRequest();
-        rawFile.overrideMimeType("application/json");
-        rawFile.open("GET", file, true);
-        rawFile.onreadystatechange = function () {
-            if (rawFile.readyState === 4 && rawFile.status == "200") {
-                callback(rawFile.responseText);
-            }
-        }
-        rawFile.send(null);
-    },
+	readFile: function (file, callback) {
+		var rawFile = new XMLHttpRequest();
+		rawFile.overrideMimeType("application/json");
+		rawFile.open("GET", file, true);
+		rawFile.onreadystatechange = function () {
+			if (rawFile.readyState === 4 && rawFile.status == "200") {
+				callback(rawFile.responseText, file);
+			}
+		}
+		rawFile.send(null);
+	},
 
-    loadFramework: function (rpath) {
-        var script = document.createElement("script");
-        script.src = rpath;
-        document.body.appendChild(script);
-    },
+	loadFramework: function (rpath) {
+		var script = document.createElement("script");
+		script.src = rpath;
+		document.body.appendChild(script);
+	},
 
-    POSIXPath: function (vpath, callback) {
-        this.readFile("filesystem.json", function (fileData) {
-            var fs = JSON.parse(fileData);
-            var vp = vpath.toString().split("/");
-            var vpl = vp.length;
-            var cpath = fs;
-            for (var i = 0; i < vpl; i++) {
-                if (vp[0] == "" && i == 0) {
-                    cpath = fs[bootVolume];
-                } else {
-                    cpath = cpath[vp[i]];
-                }
-            }
-            callback(cpath);
-        });
-    },
+	mountFilesystem: function (callback) {
+		var fsCheckDidFail = false;
+		kernel.log("Mounting filesystem...");
+		if (localStorage.getItem("filesystem") == null) {
+			kernel.log("No filesystem cache found.");
+			fsCheckDidFail = true;
+		} else {
+			kernel.log("Filesystem already cached.");
+			kernel.log("Checking file archive tree...");
+			try {
+				JSON.parse(localStorage.getItem("filesystem"));
+				kernel.log("Filesystem valid.");
+			} catch (e) {
+				kernel.log("Filesystem cache is not bootable.");
+				fsCheckDidFail = true;
+			}
+			
+		}
+		if (fsCheckDidFail == true) {
+			kernel.log("Initializing new filesystem...");
+			this.readFile("filesystem.json", function (fileData, fpath) {
+				localStorage.setItem("filesystem", fileData);
+				if (fileData == localStorage.getItem("filesystem")) {
+					kernel.log("Filesystem created successfully.");
+					callback();
+				} else {
+					kernel.log("Filesystem creation failed.");
+				}
+			});
+		} else {
+			callback();
+		}
+	},
 
-    boot: function () {
-        document.body.innerHTML = '<div id="bootScreen"><img src="boot/bootLogo.svg" /><progress value="0"></progress></div>';
-        
-        kernel.readFile("filesystem.json", function (fileData) {
-            var fs = JSON.parse(fileData);
-                var startupChime = new Audio("boot/startupChime.mp3");
-                startupChime.play();
-                setTimeout(function () {
-                    document.getElementById("bootScreen").style.opacity = 1;
-                    setTimeout(function () {
-                    document.querySelector("#bootScreen progress").style.visibility = "visible";
-                    document.querySelector("#bootScreen progress").value = 0.2;
-                            kernel.readFile("boot/bootfiles.json", function (fwData) {
-                                document.querySelector("#bootScreen progress").value = 0.4;
-                                var fwList = JSON.parse(fwData);
-                                document.querySelector("#bootScreen progress").value = 0.6;
-                                var fwListLength = fwList.load.length;
-                                document.querySelector("#bootScreen progress").value = 0.8;
-                                for (var i = 0; i < fwListLength; i++) {
-                                    kernel.POSIXPath(fwList.load[i], function (fwrPath) {
-                                        if (fwrPath.type == 1) {
-                                            kernel.loadFramework(fwrPath.path);
-                                        } else {
-                                            kernel.readFile(fwrPath.path + "/Info.json", function (infoDotJSON) {
-                                                var appInfo = JSON.parse(infoDotJSON);
-                                                kernel.loadFramework(fwrPath.path + "/MacOS/" + appInfo.CFBundleExecutable + ".js");
-                                            });
-                                        }
-                                    });
-                                }
-                        });
-                     }, 3000);
-                }, 4000);
-        });
-    }
+	POSIXPath: function (vpath) {
+		try {
+			var fs = JSON.parse(localStorage.getItem("filesystem"));
+			var vp = vpath.toString().split("/");
+			var vpl = vp.length;
+			var cpath = fs;
+			for (var i = 0; i < vpl; i++) {
+				if (vp[0] == "" && i == 0) {
+					cpath = fs[bootVolume];
+				} else {
+					cpath = cpath[vp[i]];
+				}
+			}
+			return cpath;
+		} catch (e) {
+			kernel.log(vpath + ": No such file or directory.");
+			return false;
+		}
+	},
+
+	boot: function () {
+		document.body.removeAttribute("onclick");
+		kernel.log("Initiating boot sequence...");
+		kernel.log("Checking Firmware...");
+		var startupChime = new Audio("boot/startupChime.mp3");
+		startupChime.play();
+		kernel.log("Firmware check passed.");
+		kernel.log("Detecting displays...");
+		document.body.innerHTML = '<div id="bootScreen"><img src="boot/bootLogo.svg" /><progress value="0"></progress></div>';
+		kernel.log("Detected internal display: " + window.innerWidth + " x " + window.innerHeight + "px.");
+		kernel.mountFilesystem(function () {
+			kernel.log("Proceeding with boot process...");
+			kernel.log("Boot volume is “" + bootVolume + "”.");
+			setTimeout(function () {
+				kernel.log("Checking GPU...");
+				document.getElementById("bootScreen").style.opacity = 1;
+				kernel.log("GPU check passed.");
+				setTimeout(function () {
+					kernel.log("Proceeding to load system frameworks...");
+					document.querySelector("#bootScreen progress").style.visibility = "visible";
+					document.querySelector("#bootScreen progress").value = 0.2;
+					kernel.readFile("boot/bootfiles.json", function (fwData, fwpath) {
+						document.querySelector("#bootScreen progress").value = 0.4;
+						var fwList = JSON.parse(fwData);
+						document.querySelector("#bootScreen progress").value = 0.6;
+						var fwListLength = fwList.load.length;
+						document.querySelector("#bootScreen progress").value = 0.8;
+						for (var i = 0; i < fwListLength; i++) {
+							var fwrPath = kernel.POSIXPath(fwList.load[i]);
+							if (fwrPath.type == 1) {
+								kernel.loadFramework(fwrPath.path);
+								kernel.log("Successfully loaded framework “" + fwrPath.path.split("/")[1].split(".")[0] + "”.");
+							} else {
+								kernel.readFile(fwrPath.path + "/Contents/Info.json", function (infoDotJSON, pth) {
+									var appInfo = JSON.parse(infoDotJSON);
+									kernel.loadFramework(pth.split("/Info.json")[0] + "/MacOS/" + appInfo.CFBundleExecutable + ".js");
+									kernel.log("Launching “" + pth.split("/Contents/Info.json")[0].split("/")[1] + "”...");
+								});
+							}
+						}
+					});
+				}, 2000);
+			}, 4000);
+		});
+	}
 
 }
--- projects/fake-sur/js/AppKit.js
+++ projects/fake-sur/js/AppKit.js
@@ -1,99 +1,162 @@
-function dragElement(elmnt, titlebar) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (titlebar == true) {
-        elmnt.querySelector('.titlebar').onmousedown = dragMouseDown;
-    } else {
-        elmnt.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        //e.preventDefault();
-        //e.stopPropagation();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-        elmnt.querySelector('.contentView').style.pointerEvents = "none";
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        //e.preventDefault();
-        //e.stopPropagation();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        if ((elmnt.offsetTop - pos2) < 25 || (elmnt.offsetTop - pos2) > (window.innerHeight - e.offsetHeight)) {
-            pos2 = 0;
-        }
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-        elmnt.querySelector('.contentView').style.pointerEvents = "initial";
-    }
-}
+var NSSymbolButtonType = 0;
+var NSImageButtonType = 1;
 
 function NSWindow(options) {
-    var windowBase = document.createElement("div");
-    if (options.position == "center") {
-        windowBase.style.left = ((window.innerWidth / 2) - (options.size[0] / 2)) + "px";
-        windowBase.style.top = ((window.innerHeight / 2) - (options.size[1] / 2)) + "px";
-        windowBase.style.width = options.size[0] + "px";
-        windowBase.style.height = options.size[1] + "px";
-    } else if (options.size == "maximize") {
-        windowBase.style.width = window.innerWidth + "px";
-        windowBase.style.height = ((window.innerHeight - 31) - document.getElementById('dock').offsetHeight) + "px";
-        windowBase.style.top = "25px";
-        windowBase.style.left = "0px";
-    } else {
-        windowBase.style.left = options.position[0] + "px";
-        windowBase.style.top = options.position[1] + "px";
-        windowBase.style.width = options.size[0] + "px";
-        windowBase.style.height = options.size[1] + "px";
-    }
-    windowBase.setAttribute("class", "window");
+	function dragElement(elmnt, titlebar) {
+		var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+		if (titlebar == true) {
+			elmnt.querySelector('.titlebar').onmousedown = dragMouseDown;
+		} else {
+			elmnt.onmousedown = dragMouseDown;
+		}
 
-    try {
-        windowBase.innerHTML += options.view;
-    } catch (e) { }
+		function dragMouseDown(e) {
+			e = e || window.event;
+			//e.preventDefault();
+			//e.stopPropagation();
+			// get the mouse cursor position at startup:
+			pos3 = e.clientX;
+			pos4 = e.clientY;
+			document.onmouseup = closeDragElement;
+			// call a function whenever the cursor moves:
+			document.onmousemove = elementDrag;
+			elmnt.querySelector('.contentView').style.pointerEvents = "none";
+		}
 
-    if (options.titlebar) {
-        var titlebar = document.createElement("div");
-        titlebar.setAttribute("class", "titlebar");
-        windowBase.insertBefore(titlebar, windowBase.firstChild);
-        dragElement(windowBase, true);
-        titlebar.innerHTML = '<div class="windowControls"><div class="closeButton" onclick="dockApps[' + options.app + '].close(this);"></div><div class="minimizeButton"><img src="images/minimize.svg"/></div><div class="fullscreenButton"><img src="images/fullscreen.svg"/></div></div><span class="windowTitle">' + options.title + '</span>';
-        if (options.toolbar) {
-            var toolbar = document.createElement("div");
-            toolbar.setAttribute("class", "toolbar");
-            for (var i = 0; i < options.toolbar.length; i++) {
-                toolbar.innerHTML += options.toolbar[i];
-            }
-            titlebar.appendChild(toolbar);
-            if (options.title) {
-                titlebar.setAttribute("class", "titlebarThick titled");
-            } else {
-                titlebar.setAttribute("class", "titlebarThick");
-            }
-            if (options.tabs) {
-                titlebar.classList.add("tabbed");
-            }
-        }
-    } else {
-        dragElement(windowBase, false);
-    }
+		function elementDrag(e) {
+			e = e || window.event;
+			//e.preventDefault();
+			//e.stopPropagation();
+			// calculate the new cursor position:
+			pos1 = pos3 - e.clientX;
+			pos2 = pos4 - e.clientY;
+			pos3 = e.clientX;
+			pos4 = e.clientY;
+			// set the element's new position:
+			if ((elmnt.offsetTop - pos2) < 25 || (elmnt.offsetTop - pos2) > (window.innerHeight - e.offsetHeight)) {
+				pos2 = 0;
+			}
+			elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+			elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+		}
 
-    document.body.appendChild(windowBase);
-    return windowBase;
+		function closeDragElement() {
+			/* stop moving when mouse button is released:*/
+			document.onmouseup = null;
+			document.onmousemove = null;
+			elmnt.querySelector('.contentView').style.pointerEvents = "initial";
+		}
+	}
+
+	this.HTMLElement = document.createElement("div");
+	if (options.position == "center") {
+		this.HTMLElement.style.left = ((window.innerWidth / 2) - (options.size[0] / 2)) + "px";
+		this.HTMLElement.style.top = ((window.innerHeight / 2) - (options.size[1] / 2)) + "px";
+		this.HTMLElement.style.width = options.size[0] + "px";
+		this.HTMLElement.style.height = options.size[1] + "px";
+	} else if (options.size == "maximize") {
+		this.HTMLElement.style.width = window.innerWidth + "px";
+		this.HTMLElement.style.height = ((window.innerHeight - 31) - document.getElementById('dock').offsetHeight) + "px";
+		this.HTMLElement.style.top = "25px";
+		this.HTMLElement.style.left = "0px";
+	} else {
+		this.HTMLElement.style.left = options.position[0] + "px";
+		this.HTMLElement.style.top = options.position[1] + "px";
+		this.HTMLElement.style.width = options.size[0] + "px";
+		this.HTMLElement.style.height = options.size[1] + "px";
+	}
+	this.HTMLElement.setAttribute("class", "window");
+
+	try {
+		this.HTMLElement.innerHTML += options.view;
+	} catch (e) { }
+
+	if (options.titlebar) {
+		this.titlebar = document.createElement("div");
+		this.titlebar.setAttribute("class", "this.titlebar");
+		this.HTMLElement.insertBefore(this.titlebar, this.HTMLElement.firstChild);
+		dragElement(this.HTMLElement, true);
+		this.titlebar.innerHTML = '<div class="windowControls"><div class="closeButton" onclick="dockApps[' + options.app + '].close(this);"></div><div class="minimizeButton"><img src="images/minimize.svg"/></div><div class="fullscreenButton"><img src="images/fullscreen.svg"/></div></div><span class="windowTitle">' + options.title + '</span>';
+	} else {
+		dragElement(this.HTMLElement, false);
+	}
+
+	document.body.appendChild(this.HTMLElement);
+	return this;
+}
+
+function NSToolbar(options) {
+	this.HTMLElement = document.createElement("div");
+	this.HTMLElement.setAttribute("class", "toolbar");
+	for (var i = 0; i < options.items.length; i++) {
+		this.HTMLElement.innerHTML += options.items[i];
+	}
+	this.HTMLElement.appendChild(toolbar);
+	if (options.title) {
+		this.HTMLElement.setAttribute("class", "titlebarThick titled");
+	} else {
+		this.HTMLElement.setAttribute("class", "titlebarThick");
+	}
+	if (options.tabs) {
+		this.HTMLElement.classList.add("tabbed");
+	}
+	return this;
+}
+
+function NSButton(options) {
+	if (options.type == NSToolbarSpace) {
+		this.HTMLElement = document.createElement("div");
+		this.HTMLElement.classList.add("toolbarspace");
+	} else {
+		this.HTMLElement = document.createElement("a");
+		this.HTMLElement.href = "javascript:void(0);";
+		switch (options.type) {
+			case NSSymbolButtonType:
+				this.HTMLElement.classList.add("toolbarbutton");
+				this.HTMLElement.classList.add("f7-icons");
+				this.HTMLElement.setAttribute("onclick", options.action);
+				break;
+			case NSImageType:
+				this.HTMLElement.innerHTML = options.image;
+				break;
+		}
+		try {
+			this.HTMLElement.classList.add(options.externalClasses);
+		} catch (e) { }
+		this.HTMLElement.innerHTML = options.title;
+	}
+	return this;
+}
+
+function NSSpacerElement(options) {
+	this.HTMLElement = document.createElement("div");
+	this.HTMLElement.classList.add("toolbarspace");
+	return this;
+}
+
+function NSImage(options) {
+	this.HTMLElement = document.createElement("img");
+	this.HTMLElement.src = options.src;
+}
+
+function NSDockTile(options) {
+	this.path = options.path;
+	this.name = options.name;
+	this.icon = options.icon;
+	this.HTMLElement = document.createElement("div");
+	this.HTMLElement.classList = "docktile tooltip";
+	if (options.type == "app") {
+		this.HTMLElement.onclick = function () {
+			this.classList.add("launching");
+			kernel.exec(options.path);
+		}
+		this.HTMLElement.innerHTML = '<span class="tooltiptext">' + options.name + '</span><img src="' + options.path + "/Contents/Resources/" + options.icon + '/icon_128x128.png" \/>';
+	} else {
+		this.HTMLElement.onclick = function () {
+			kernel.exec(options.path);
+		}
+		this.HTMLElement.innerHTML = '<span class="tooltiptext">' + options.name + '</span><img src="' + options.icon + '/icon_128x128.png" \/>';
+	}
+
+	return this;
 }
