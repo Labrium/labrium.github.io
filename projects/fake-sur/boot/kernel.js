e760f625b20b0afedf59dbb32804653c87f16936
TechLabsInc <76017200+TechLabsInc@users.noreply.github.com>
2021-05-04 18:34:28 -0600

    Added window resizing from any edge or corner, updated OS compiler to split filesystem into fragments less than 100MB

--- projects/fake-sur/boot/kernel.js
+++ projects/fake-sur/boot/kernel.js
@@ -73,7 +73,11 @@ var kernel = {
 			kernel.log("External system image found. Attempting to boot...");
 			kernel.log("Checking file archive tree...");
 			try {
-				JSON.parse(ROOTFS);
+				if (typeof ROOTFS == "string") {
+					JSON.parse(ROOTFS);
+				} else {
+					kernel.POSIXPath("/System");
+				}
 				kernel.log("External system image is valid.");
 			} catch (e) {
 				kernel.log("External system image is not bootable.");
@@ -82,30 +86,31 @@ var kernel = {
 		}
 		if (fsCheckDidFail == true) {
 			kernel.log("Initializing default OS...");
-			this.readFile("filesystem.json", function (fileData, fpath) {
-				try {
-					ROOTFS = fileData;
-				} catch (e) {
-					kernel.log("Error: Disk space not sufficient for fakeOS.");
-				}
-				if (fileData == ROOTFS) {
-					kernel.log("Filesystem mounted successfully.");
-					kernel.log("Checking file archive tree...");
-					try {
-						JSON.parse(ROOTFS);
-						kernel.log("Filesystem is valid.");
-						callback();
-					} catch (e) {
-						kernel.log("Filesystem is not bootable.");
-						kernel.log("Filesystem mount failed.");
-						kernel.log("Shutting Down...");
-						kernel.shutdown();
+			kernel.readFile("OS.fsimage/Contents.json", function (fraglist, fraglistpath) {
+				var i = 0;
+				var frags = JSON.parse(fraglist)["Contents"];
+				ROOTFS = "";
+				function loadFSFrags(frag, fragpath) {
+					ROOTFS += frag;
+					i++;
+					if (frags[i]) {
+						kernel.readFile("OS.fsimage/" + frags[i], loadFSFrags);
+					} else {
+						kernel.log("Filesystem mounted successfully.");
+						kernel.log("Checking file archive tree...");
+						try {
+							JSON.parse(ROOTFS);
+							kernel.log("Filesystem is valid.");
+							callback();
+						} catch (e) {
+							kernel.log("Filesystem is not bootable.");
+							kernel.log("Filesystem mount failed.");
+							kernel.log("Shutting Down...");
+							kernel.shutdown();
+						}
 					}
-				} else {
-					kernel.log("Filesystem mount failed.");
-					kernel.log("Shutting Down...");
-					kernel.shutdown();
 				}
+				kernel.readFile("OS.fsimage/" + frags[0], loadFSFrags);
 			});
 		} else {
 			callback();
@@ -128,13 +133,15 @@ var kernel = {
 			return cpath;
 		} catch (e) {
 			try {
-				var fs = JSON.parse(ROOTFS);
+				if (typeof ROOTFS == "string") {
+					ROOTFS = JSON.parse(ROOTFS);
+				}
 				var vp = vpath.toString().split("/");
 				var vpl = vp.length;
-				var cpath = fs;
+				var cpath = ROOTFS;
 				for (var i = 0; i < vpl; i++) {
 					if (vp[0] == "" && i == 0) {
-						cpath = fs[BOOTVOLUME];
+						cpath = ROOTFS[BOOTVOLUME];
 					} else {
 						cpath = cpath[vp[i]];
 					}
@@ -153,7 +160,7 @@ var kernel = {
 		var PTS = 0;
 		var PCS = 0;
 		function upb() {
-			document.querySelector("#bootScreen progress").value = (PCS/PTS) * 0.5;
+			document.querySelector("#bootScreen progress").value = (PCS / PTS) * 0.5;
 		}
 		document.body.removeAttribute("onclick");
 		kernel.log("Initiating boot sequence...");
@@ -177,28 +184,28 @@ var kernel = {
 					var fwData = kernel.POSIXPath("/var/db/bootfiles.json")["@CONTENTS"];
 					var fwList = JSON.parse(fwData);
 					var fwListLength = fwList.load.length;
-					PTS = fwListLength+1;
+					PTS = fwListLength + 1;
 					PCS = 1;
 					var i = 0;
 					function lf() {
 						try {
-						if (kernel.POSIXPath(fwList.load[i])["@TYPE"] == 1) {
-							var fwrPath = kernel.POSIXPath(fwList.load[i]);
-							kernel.loadFramework(fwrPath["@CONTENTS"]);
-							kernel.log("Successfully loaded framework “" + fwList.load[i].split("/")[fwList.load[i].split("/").length - 1].split(".")[0] + "”.");
-						} else {
-							var infoDotJSON = kernel.POSIXPath(fwList.load[i] + "/Contents/Info.json")["@CONTENTS"];
-							var appInfo = JSON.parse(infoDotJSON);
-							kernel.log("Launching “" + fwList.load[i].split("/")[fwList.load[i].split("/").length - 1] + "”...");
-							kernel.loadFramework(kernel.POSIXPath(fwList.load[i] + "/Contents/MacOS/" + appInfo.CFBundleExecutable + ".js")["@CONTENTS"]);
-						}
-						document.querySelector("#bootScreen progress").value += 0.3;
-						i++;
-						PCS = i+1;
-						upb();
-						if (i <= fwListLength) {
-							setTimeout(lf, 0);
-						}
+							if (kernel.POSIXPath(fwList.load[i])["@TYPE"] == 1) {
+								var fwrPath = kernel.POSIXPath(fwList.load[i]);
+								kernel.loadFramework(fwrPath["@CONTENTS"]);
+								kernel.log("Successfully loaded framework “" + fwList.load[i].split("/")[fwList.load[i].split("/").length - 1].split(".")[0] + "”.");
+							} else {
+								var infoDotJSON = kernel.POSIXPath(fwList.load[i] + "/Contents/Info.json")["@CONTENTS"];
+								var appInfo = JSON.parse(infoDotJSON);
+								kernel.log("Launching “" + fwList.load[i].split("/")[fwList.load[i].split("/").length - 1] + "”...");
+								kernel.loadFramework(kernel.POSIXPath(fwList.load[i] + "/Contents/MacOS/" + appInfo.CFBundleExecutable + ".js")["@CONTENTS"]);
+							}
+							document.querySelector("#bootScreen progress").value += 0.3;
+							i++;
+							PCS = i + 1;
+							upb();
+							if (i <= fwListLength) {
+								setTimeout(lf, 0);
+							}
 						} catch (e) {
 							BOOTING = false;
 						}
